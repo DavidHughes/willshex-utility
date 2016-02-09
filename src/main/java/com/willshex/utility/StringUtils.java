584cc614ef894bed91b534eb09a7fb0e82f2cc6b
billy1380 <william.shakour@gmail.com>
2016-02-09 15:41:41 +0000

    added some javadoc to string utilities

--- src/main/java/com/willshex/utility/StringUtils.java
+++ src/main/java/com/willshex/utility/StringUtils.java
@@ -17,52 +17,91 @@ import java.security.NoSuchAlgorithmException;
  */
 public class StringUtils {
 
-	private static final String[] ESCAPE_CHARS = { " ", "{", "}", ";", "/", "?", ":", "@", "&", "=", "+", "$", ",", "[", "]", "#", "!", "'", "(", ")", "*",
-			"\"", "<", ">" };
+	private static final String[] ESCAPE_CHARS = { " ", "{", "}", ";", "/", "?",
+			":", "@", "&", "=", "+", "$", ",", "[", "]", "#", "!", "'", "(",
+			")", "*", "\"", "<", ">" };
 
-	private static final String[] REPLACE_CHARS = { "%20", "%7B", "%7D", "%3B", "%2F", "%3F", "%3A", "%40", "%26", "%3D", "%2B", "%24", "%2C", "%5B", "%5D",
-			"%23", "%21", "%27", "%28", "%29", "%2A", "%22", "%3C", "%3E" };
+	private static final String[] REPLACE_CHARS = { "%20", "%7B", "%7D", "%3B",
+			"%2F", "%3F", "%3A", "%40", "%26", "%3D", "%2B", "%24", "%2C",
+			"%5B", "%5D", "%23", "%21", "%27", "%28", "%29", "%2A", "%22",
+			"%3C", "%3E" };
 
-	public static String sanitise(String value) {
+	public static String sanitise (String value) {
 		return value;
 		// value.replace("<", "").replace(">", "");
 	}
 
-	public static String stripslashes(String value) {
-		return value == null ? null : value.replace("\\'", "'").replace("\\\"", "\"").replace("\\\\", "\\");
+	/**
+	 * Strip slashes from string specifically for un-escaping slashes (\) and single quotes (')
+	 * @param value string to strip
+	 * @return striped string
+	 */
+	public static String stripslashes (String value) {
+		return value == null ? null
+				: value.replace("\\'", "'").replace("\\\"", "\"")
+						.replace("\\\\", "\\");
 	}
 
-	public static String addslashes(String value) {
-		return value == null ? null : value.replace("\\", "\\\\").replace("\"", "\\\"").replace("'", "\\'");
+	/**
+	 * Strip slashes from string specifically for escaping slashes (\) and single quotes (')
+	 * @param value string to escape
+	 * @return escaped string
+	 */
+	public static String addslashes (String value) {
+		return value == null ? null
+				: value.replace("\\", "\\\\").replace("\"", "\\\"").replace("'",
+						"\\'");
 	}
 
-	public static String urldecode(String value) {
+	/**
+	 * Url decode replaces codes "%20", "%7B", "%7D", "%3B",
+			"%2F", "%3F", "%3A", "%40", "%26", "%3D", "%2B", "%24", "%2C",
+			"%5B", "%5D", "%23", "%21", "%27", "%28", "%29", "%2A", "%22",
+			"%3C", "%3E" with " ", "{", "}", ";", "/", "?",
+			":", "@", "&", "=", "+", "$", ",", "[", "]", "#", "!", "'", "(",
+			")", "*", "\"", "<", ">"
+	 * @param value string to decode
+	 * @return decoded string
+	 */
+	public static String urldecode (String value) {
 		StringBuffer replaced = new StringBuffer(value);
 
 		int i, start;
 		for (i = 0; i < REPLACE_CHARS.length; i++) {
 			while ((start = replaced.indexOf(REPLACE_CHARS[i])) >= 0) {
-				replaced.replace(start, start + REPLACE_CHARS[i].length(), ESCAPE_CHARS[i]);
+				replaced.replace(start, start + REPLACE_CHARS[i].length(),
+						ESCAPE_CHARS[i]);
 			}
 		}
 
 		return replaced.toString();
 	}
 
-	public static String urlencode(String value) {
+	/**
+	 * Url encode replaces " ", "{", "}", ";", "/", "?",
+			":", "@", "&", "=", "+", "$", ",", "[", "]", "#", "!", "'", "(",
+			")", "*", "\"", "<", ">" with codes "%20", "%7B", "%7D", "%3B",
+			"%2F", "%3F", "%3A", "%40", "%26", "%3D", "%2B", "%24", "%2C",
+			"%5B", "%5D", "%23", "%21", "%27", "%28", "%29", "%2A", "%22",
+			"%3C", "%3E"
+	 * @param value string to encode
+	 * @return encoded string
+	 */
+	public static String urlencode (String value) {
 		StringBuffer replaced = new StringBuffer(value);
 
 		int i, start;
 		for (i = 0; i < ESCAPE_CHARS.length; i++) {
 			while ((start = replaced.indexOf(ESCAPE_CHARS[i])) >= 0) {
-				replaced.replace(start, start + ESCAPE_CHARS[i].length(), REPLACE_CHARS[i]);
+				replaced.replace(start, start + ESCAPE_CHARS[i].length(),
+						REPLACE_CHARS[i]);
 			}
 		}
 
 		return replaced.toString();
 	}
 
-	public static String sha1Hash(String content) {
+	public static String sha1Hash (String content) {
 		byte[] bytesOfMessage = null;
 		byte[] theDigest = null;
 
@@ -80,7 +119,7 @@ public class StringUtils {
 		return theDigest == null ? null : convertDigestToString(theDigest);
 	}
 
-	public static String md5Hash(String content) {
+	public static String md5Hash (String content) {
 		byte[] bytesOfMessage = null;
 		byte[] theDigest = null;
 
@@ -98,16 +137,18 @@ public class StringUtils {
 		return theDigest == null ? null : convertDigestToString(theDigest);
 	}
 
-	private static String convertDigestToString(byte[] digest) {
+	private static String convertDigestToString (byte[] digest) {
 		StringBuffer result = new StringBuffer();
 		for (int i = 0; i < digest.length; i++) {
-			result.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+			result.append(Integer.toString((digest[i] & 0xff) + 0x100, 16)
+					.substring(1));
 		}
 
 		return result.toString();
 	}
 
-	public static String join(Iterable<? extends CharSequence> items, CharSequence glue) {
+	public static String join (Iterable<? extends CharSequence> items,
+			CharSequence glue) {
 		StringBuffer buffer = new StringBuffer();
 
 		for (CharSequence item : items) {
@@ -121,14 +162,19 @@ public class StringUtils {
 		return buffer.toString();
 	}
 
-	public static String join(Iterable<? extends CharSequence> items) {
+	public static String join (Iterable<? extends CharSequence> items) {
 		return join(items, ",");
 	}
 
-	private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", LOWER = "abcdefghijklmnopqrstuvwxyz";
+	private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+			LOWER = "abcdefghijklmnopqrstuvwxyz";
 
-	public static String rot13(String value) {
-
+	/**
+	 * Replace every alpha character in a string with the character 13 over  
+	 * @param value string to rotate
+	 * @return rotated result
+	 */
+	public static String rot13 (String value) {
 		StringBuffer buffer = new StringBuffer(value.length());
 
 		int count = value.length();
@@ -148,21 +194,33 @@ public class StringUtils {
 
 		return buffer.toString();
 	}
-	
-	public static String upperCaseFirstLetter(String value) {
+
+	/**
+	 * Make the first character of the string upper-case
+	 * @param value string to process
+	 * @return string with upper-case first letter
+	 */
+	public static String upperCaseFirstLetter (String value) {
 		String upperCaseFirstLetter = null;
 
 		String firstLetter = value.substring(0, 1);
-		upperCaseFirstLetter = value.replaceFirst(firstLetter, firstLetter.toUpperCase());
+		upperCaseFirstLetter = value.replaceFirst(firstLetter,
+				firstLetter.toUpperCase());
 
 		return upperCaseFirstLetter;
 	}
-	
-	public static String lowerCaseFirstLetter(String value) {
+
+	/**
+	 * Make the first character of the string lower-case
+	 * @param value string to process
+	 * @return string with lower-case first letter
+	 */
+	public static String lowerCaseFirstLetter (String value) {
 		String lowerCaseFirstLetter = null;
 
 		String firstLetter = value.substring(0, 1);
-		lowerCaseFirstLetter = value.replaceFirst(firstLetter, firstLetter.toLowerCase());
+		lowerCaseFirstLetter = value.replaceFirst(firstLetter,
+				firstLetter.toLowerCase());
 
 		return lowerCaseFirstLetter;
 	}
