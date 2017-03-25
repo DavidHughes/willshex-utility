bcbf6e907edf140c816a7877976af60468233bfa
billy1380 <william.shakour@gmail.com>
2017-03-25 16:47:11 +0000

    added base64 and unbase64 to string class

--- src/main/java/com/willshex/utility/StringUtils.java
+++ src/main/java/com/willshex/utility/StringUtils.java
@@ -7,6 +7,7 @@
 //
 package com.willshex.utility;
 
+import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -478,5 +479,184 @@ public class StringUtils {
 		constant.append(suffix);
 
 		return constant.toString();
+	}
+
+	// from http://stackoverflow.com/questions/392464/how-do-i-do-base64-encoding-on-iphone-sdk/800976#800976
+
+	static char[] base64EncodingTable = new char[] { 'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+			'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
+			'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+			's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+			'5', '6', '7', '8', '9', '+', '/' };
+
+	public static String base64 (byte[] raw) {
+		int ixtext, lentext;
+		int ctremaining;
+		byte[] input = new byte[3], output = new byte[4];
+		short i, charsonline = 0, ctcopy;
+		StringBuffer result;
+
+		lentext = raw.length;
+
+		if (lentext < 1) { return ""; }
+
+		result = new StringBuffer(lentext);
+
+		ixtext = 0;
+
+		while (true) {
+			ctremaining = lentext - ixtext;
+
+			if (ctremaining <= 0) {
+				break;
+			}
+
+			for (i = 0; i < 3; i++) {
+				int ix = ixtext + i;
+
+				if (ix < lentext) {
+					input[i] = raw[ix];
+				} else {
+					input[i] = 0;
+				}
+			}
+
+			output[0] = (byte) ((input[0] & 0xFC) >> 2);
+			output[1] = (byte) (((input[0] & 0x03) << 4)
+					| ((input[1] & 0xF0) >> 4));
+			output[2] = (byte) (((input[1] & 0x0F) << 2)
+					| ((input[2] & 0xC0) >> 6));
+			output[3] = (byte) (input[2] & 0x3F);
+
+			ctcopy = 4;
+
+			switch (ctremaining) {
+			case 1:
+				ctcopy = 2;
+				break;
+			case 2:
+				ctcopy = 3;
+				break;
+			}
+
+			for (i = 0; i < ctcopy; i++) {
+				result.append(base64EncodingTable[output[i]]);
+			}
+
+			for (i = ctcopy; i < 4; i++) {
+				result.append("=");
+			}
+
+			ixtext += 3;
+			charsonline += 4;
+
+			if ((ixtext % 90) == 0) {
+				result.append("\n");
+			}
+
+			if (raw.length > 0) {
+				if (charsonline >= raw.length) {
+					charsonline = 0;
+
+					result.append("\n");
+				}
+			}
+		}
+
+		return result.toString();
+	}
+
+	public static byte[] unbase64 (String string) {
+		int ixtext, lentext;
+		byte ch;
+		byte[] input = new byte[4], output = new byte[3];
+		short i, ixinput;
+		boolean flignore, flendtext = false;
+
+		ByteArrayOutputStream result;
+
+		if (string == null) { return new byte[0]; }
+
+		ixtext = 0;
+
+		lentext = string.length();
+
+		result = new ByteArrayOutputStream(lentext);
+
+		ixinput = 0;
+
+		while (true) {
+			if (ixtext >= lentext) {
+				break;
+			}
+
+			ch = (byte) string.charAt(ixtext++);
+
+			flignore = false;
+
+			if ((ch >= 'A') && (ch <= 'Z')) {
+				ch = (byte) (ch - 'A');
+			} else if ((ch >= 'a') && (ch <= 'z')) {
+				ch = (byte) (ch - 'a' + 26);
+			} else if ((ch >= '0') && (ch <= '9')) {
+				ch = (byte) (ch - '0' + 52);
+			} else if (ch == '+') {
+				ch = 62;
+			} else if (ch == '=') {
+				flendtext = true;
+			} else if (ch == '/') {
+				ch = 63;
+			} else {
+				flignore = true;
+			}
+
+			if (!flignore) {
+				short ctcharsinput = 3;
+				Boolean flbreak = false;
+
+				if (flendtext) {
+					if (ixinput == 0) {
+						break;
+					}
+
+					if ((ixinput == 1) || (ixinput == 2)) {
+						ctcharsinput = 1;
+					} else {
+						ctcharsinput = 2;
+					}
+
+					ixinput = 3;
+
+					flbreak = true;
+				}
+
+				input[ixinput++] = ch;
+
+				if (ixinput == 4) {
+					ixinput = 0;
+
+					byte char0 = input[0];
+					byte char1 = input[1];
+					byte char2 = input[2];
+					byte char3 = input[3];
+
+					output[0] = (byte) ((char0 << 2) | ((char1 & 0x30) >> 4));
+					output[1] = (byte) (((char1 & 0x0F) << 4)
+							| ((char2 & 0x3C) >> 2));
+					output[2] = (byte) (((char2 & 0x03) << 6) | (char3 & 0x3F));
+
+					for (i = 0; i < ctcharsinput; i++) {
+						result.write(output, i, 1);
+					}
+				}
+
+				if (flbreak) {
+					break;
+				}
+			}
+		}
+
+		return result.toByteArray();
 	}
 }
