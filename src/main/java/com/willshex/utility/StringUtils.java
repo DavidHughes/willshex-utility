5cf6ed2ab3b607fcf699b4f5d98c2d40e5f3a85e
billy1380 <william.shakour@gmail.com>
2018-11-27 23:27:19 +0000

    added is empty and is not empty string helpers

--- src/main/java/com/willshex/utility/StringUtils.java
+++ src/main/java/com/willshex/utility/StringUtils.java
@@ -812,4 +812,12 @@ public class StringUtils {
 		return matchParts;
 	}
 
+	public static boolean isEmpty (String value) {
+		return value == null || value.isEmpty();
+	}
+
+	public static boolean isNotEmpty (String value) {
+		return !isEmpty(value);
+	}
+
 }
